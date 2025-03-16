c564688f8ba0c76772f5d80312935866790002c0
smowky <michal@slezarik.sk>
2025-03-16 19:30:41 +0100

    ssh moved to main.tf

--- scaleway/dev/main.tf
+++ scaleway/dev/main.tf
@@ -56,6 +56,15 @@ module "server02" {
 }
 
 #################################################
+# SSH
+resource "scaleway_iam_ssh_key" "main" {
+  name       = "main"
+  public_key = var.ssh_public_key
+  project_id = var.project_id
+}
+
+
+#################################################
 # Volumes
 
 module "disk01" {
--- scaleway/dev/ssh.tf
+++
@@ -1,5 +0,0 @@
-resource "scaleway_iam_ssh_key" "main" {
-  name       = "main"
-  public_key = var.ssh_public_key
-  project_id = var.project_id
-}
