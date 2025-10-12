a66993f8a8548d22bb4f8c26af9027ae7a26f111
smowky <michal@slezarik.sk>
2025-10-12 19:21:29 +0200

    VPC fixed

--- scaleway/modules/vpc/vpc.tf
+++ scaleway/modules/vpc/vpc.tf
@@ -10,6 +10,7 @@ resource "scaleway_vpc" "vpc" {
 # Vritual private network
 resource "scaleway_vpc_private_network" "pn_priv" {
   name       = (var.pn_name == "" ? "pn-${var.env}-${var.server_domain}" : var.pn_name)
+  vpc_id     = scaleway_vpc.vpc.id
   tags       = [var.env, var.server_domain]
   region     = var.scaleway_region
   project_id = var.project_id
